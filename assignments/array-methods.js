5bd5a5fae20bfa3b262b0bf64044ea3b49f1e74e
Uzias Rivera <uziasrivera@gmail.com>
2019-08-20 15:59:08 -0700

    completed project

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -61,6 +61,8 @@ let fullNames = [];
 runners.forEach(element => fullNames.push(`${element.first_name} ${element.last_name}`))
 console.log(fullNames);
 
+
+
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. 
 //Populate an array called `firstNamesAllCaps`. This array will contain just strings.
@@ -72,7 +74,7 @@ console.log(firstNamesAllCaps);
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, 
 //containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
-runnersLargeSizeShirt = runners.filter(element=>element.shirt_size!=='L')
+runnersLargeSizeShirt = runners.filter(element=>element.shirt_size==='L')
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
@@ -85,6 +87,7 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+
 
 // Problem 2
 
